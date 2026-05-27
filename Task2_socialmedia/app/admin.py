3b576b6f6ed2714eee497364377763d41c31adf5
Prathiba-hub <sd1768135@gmail.com>
2026-05-27 18:52:24 +0530

    Delete Task2_socialmedia directory

--- Task2_socialmedia/app/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Profile, Post, Comment, Like, Follow
-
-admin.site.register(Profile)
-admin.site.register(Post)
-admin.site.register(Comment)
-admin.site.register(Like)
-admin.site.register(Follow)
